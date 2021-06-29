23db51be8c36920b6326ed182116e229e9350aee
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-06-29 18:08:15 -0500

    First commit

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# ------------------------------------------------------------------
+# Oromion Generate png from (LaTeX).
+#          Description
+# ------------------------------------------------------------------
+VERSION=0.1.0
+USAGE="Usage: run.sh"
+# --- Options processing -------------------------------------------
+# [ $# -eq 0 ] && { echo "Usage: $0 filename"; exit 1; }
+set -e -u
+echo "Bash version: $BASH_VERSION"
+# _number="$1"
+
+DIR=$(pwd)/build
+if [ -d "$DIR" ]; then
+  printf '%s\n' "Removing Lock ($DIR)"
+  rm -rf "$DIR"
+fi
+
+
+mkdir -p build && cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../src
+make
+zathura doc/doc_dune-webinar_tex_source.pdf
--- src/doc/.latexmkrc
+++ src/doc/.latexmkrc
@@ -14,8 +14,8 @@ $postscript_mode = $dvi_mode = 0;
 # -synctex=1:
 # -interaction=nonstopmode:
 # -file-line-error:
-$pdflatex = 'lualatex %O %S --shell-escape -interaction=batchmode -draftmode';
-$pdflatex = 'lualatex %O %S --shell-escape -interaction=batchmode -draftmode';
-$pdflatex = 'lualatex %O %S --shell-escape -interaction=batchmode -synctex=1';
+$pdflatex = 'lualatex %O %S -shell-escape -interaction=batchmode -draftmode';
+$pdflatex = 'lualatex %O %S -shell-escape -interaction=batchmode -draftmode';
+$pdflatex = 'lualatex %O %S -shell-escape -interaction=batchmode -synctex=1';
 
 $clean_ext = "aux fdb_latexmk fls lof log lol run.xml out toc bbl blg bcf";
