3435c8c26122a971bd5db8538300fa4c995f7d35
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-07-04 19:48:55 -0500

    Avance

--- run.sh
+++ run.sh
@@ -21,4 +21,5 @@ fi
 mkdir -p build && cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../src
 make
-evince doc/doc_dune-webinar_tex_source.pdf
+VISOR=zathura
+${VISOR} doc/doc_dune-webinar_tex_source.pdf
