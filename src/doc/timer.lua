82be181ed88a4daa6be2699235bb60058b09f6dc
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-07-11 20:49:24 -0500

    Update

---
+++ src/doc/timer.lua
@@ -0,0 +1,9 @@
+--timer.lua:
+sum = 0
+
+function difference(increment)
+oldsumstr = string.format("%.2f",sum/60)
+sum = sum + increment
+sumstr = string.format("%.2f",sum/60)
+return oldsumstr,sumstr
+end
