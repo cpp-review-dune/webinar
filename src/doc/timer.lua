25f3b6e0f6653275e6e51a61b6a0831ae0ab339c
Carlos Aznarán Laos <caznaranl@uni.pe>
2022-04-15 17:08:07 -0500

    Update

--- src/doc/timer.lua
+++ src/doc/timer.lua
@@ -1,9 +1,12 @@
+#!/usr/bin/env lua
+
 --timer.lua:
 sum = 0
 
 function difference(increment)
-oldsumstr = string.format("%.2f",sum/60)
-sum = sum + increment
-sumstr = string.format("%.2f",sum/60)
-return oldsumstr,sumstr
+  oldsumstr = string.format("%.2f",sum/60)
+  sum = sum + increment
+  sumstr = string.format("%.2f",sum/60)
+
+  return oldsumstr,sumstr
 end
